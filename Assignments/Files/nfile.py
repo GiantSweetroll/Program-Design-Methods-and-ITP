d4129a33a80ef45419c459bd392c9fb1b456d513
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2019-12-11 07:25:12 +0700

    Finished nfile.py

--- Assignments/Files/nfile.py
+++ Assignments/Files/nfile.py
@@ -1,2 +1,13 @@
-class NFile():
-    
+import io
+f = io.open("textbook.txt", encoding = "utf8")
+contents = f.readlines()
+f.close()
+
+f2 = io.open("lined_textbook.txt", "a", encoding = "utf8")
+
+i = 1
+
+for content in contents:
+    f2.write(str(i) + ". " + content)
+    i+=1
+f2.close()
