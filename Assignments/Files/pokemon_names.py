4daf3a45cc3df9511066e48a5b33fd276a8c3d7c
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2019-12-12 21:25:44 +0700

    Algo no longer operated on random, but uses brute force
    Process is long

--- Assignments/Files/pokemon_names.py
+++ Assignments/Files/pokemon_names.py
@@ -1,5 +1,5 @@
+import copy
 import io
-import random
 
 
 f = io.open("pokemonNames.txt")
@@ -7,38 +7,60 @@ f = io.open("pokemonNames.txt")
 contents = f.read()
 f.close()
 
-def getChain() -> [str]:
-    #Separate to first letter dictionary
-    names:[] = contents.split()
-    firstNames:{} = {}
-    
+names:[] = contents.split()
+firstNames:{} = {}
+mutableFirstNames:{} = {}
+allCombinations:[] = []
+
+def createFirstLetterDictionary():
     for name in names:
         firstLetter:str = name[0:1]
         if firstLetter in firstNames:
             firstNames[firstLetter].append(name)
         else:
             firstNames[firstLetter] = [name]
-    
-    chain:[str] = []
-    i:str = random.choice(list(firstNames.keys()))
-    while(len(firstNames) != 0):
-        try:
-            ls = list(firstNames[i])
-        except KeyError:
-            break
-        
-        if len(ls) != 0:
-            index = random.randint(0, len(ls)-1)
-            
-            name:str = ls[index]
-            chain.append(name)
-            
-            del(firstNames[i][index])
-            
-            i = name[-1:]
-        else:
-            break
-    return chain
 
-for i in range(10):
-    print(getChain())
+def addChainToAllCombinationsList(chain):
+    allCombinations.append(chain.split())
+def getPokemonNameList(firstLetter:str, index:int, currentChain, remainingNames:{}):
+    if firstLetter in remainingNames and index < len(remainingNames[firstLetter]):
+        name:str = remainingNames[firstLetter][index]
+        copyRemNames:{} = copy.deepcopy(remainingNames)
+        del(copyRemNames[firstLetter][index])
+        currentChain += " " + name
+        firstLetter = name[-1:]
+###         print(currentChain)
+        if firstLetter not in copyRemNames:
+#             print(firstLetter, "not in dictionary 1")
+            addChainToAllCombinationsList(currentChain)
+        else:
+            length:int = len(copyRemNames[firstLetter])
+            if length > 0:
+                for i in range(length):
+                    getPokemonNameList(firstLetter, i, currentChain, copyRemNames)
+            else:
+#                 print("length is 0")
+                addChainToAllCombinationsList(currentChain)    
+    else:
+#         print(firstLetter, "not in dictionary 2")
+        addChainToAllCombinationsList(currentChain)  
+
+#Start
+createFirstLetterDictionary()
+mutableFirstNames = firstNames
+for letter in firstNames:
+    for i in range(len(firstNames[letter])):
+        getPokemonNameList(letter, i, "", mutableFirstNames)
+
+#Find longest chain
+index:int = 0
+highest: int = 0
+for i in range(len(allCombinations)):
+    if len(allCombinations[i]) > highest:
+        highest = len(allCombinations[i])
+        index = i
+
+#Print longest chain
+print("Longest chain:", highest)
+for name in allCombinations[index]:
+    print(name)
