c592da5d224157a3a1acaf5fcbd830c6468a3eae
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2020-01-04 11:22:36 +0700

    Update globals.py
    More global variables

--- final_project/handwritting_recognition/pygame/globals.py
+++ final_project/handwritting_recognition/pygame/globals.py
@@ -1,2 +1,7 @@
+from final_project.handwritting_recognition.pygame.data_drivers.ai import AI
+
 mouse_left_pressed:bool = False
 panel_index:int = 0
+active_ai:AI = None
+loading_active:bool = False
+loading_progress:str = ""
