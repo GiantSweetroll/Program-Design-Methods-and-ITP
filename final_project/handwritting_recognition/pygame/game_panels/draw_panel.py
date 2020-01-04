f3af0995d309fef04b0c9c38307bc7f7c017d997
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2020-01-04 11:20:47 +0700

    DrawPanelGame created, configured DrawPanel accordingly

--- final_project/handwritting_recognition/pygame/game_panels/draw_panel.py
+++ final_project/handwritting_recognition/pygame/game_panels/draw_panel.py
@@ -1,91 +1,73 @@
+from abc import abstractclassmethod
 import sys
 
 import pygame
 from pygame.event import Event
 from pygame.rect import Rect
 
-from final_project.handwritting_recognition import constants, file_operation, \
-    methods
-from final_project.handwritting_recognition.neural_network import NeuralNetwork
+from final_project.handwritting_recognition import constants
 from final_project.handwritting_recognition.pygame import game_functions as gf
+from final_project.handwritting_recognition.pygame import globals
 from final_project.handwritting_recognition.pygame.buttons.button import Button
 from final_project.handwritting_recognition.pygame.game_panels.panel import Panel
+from final_project.handwritting_recognition.pygame.labels.label import Label
 from final_project.handwritting_recognition.pygame.settings import Settings
 
 
 class DrawPanel(Panel):
     
     #Constructor
-    def __init__(self, screen, settings:Settings, neural_network:NeuralNetwork = None):
+    def __init__(self, screen, settings:Settings, sub_title_text:str = ""):
         super().__init__(screen, settings)
         
         #Fields initialization
-        self.__exit_button:Button = Button(self.get_screen(), self.get_settings(), "Exit", button_color = constants.color_red)
-        self.__guess_button:Button = Button(self.get_screen(), self.get_settings(), "Guess")
-        self.__clear_button:Button = Button(self.get_screen(), self.get_settings(), "Clear")
-        self.__buttons_to_draw:[Button] =   [self.__exit_button, self.__guess_button, self.__clear_button]
-        self.__guess_button_pressed:boolean = False
+        self._exit_button:Button = Button(self.get_screen(), self.get_settings(), "Exit", button_color = constants.color_red)
+        self._guess_button:Button = Button(self.get_screen(), self.get_settings(), "Guess")
+        self._clear_button:Button = Button(self.get_screen(), self.get_settings(), "Clear")
+        self._buttons_to_draw:[Button] =   [self._exit_button, self._guess_button, self._clear_button]
+        self._guess_button_pressed:boolean = False
         self.__colored_pixels = set(())
-        self.__neural_network = neural_network
+        self._sub_title_label:Label = Label(sub_title_text)
+        self._draw_sub_title_label:bool = False
         
         #Configure button placements
+        screen_rect = self.get_screen().get_rect()
+        #Sub title
+        self._sub_title_label.get_rect().top = screen_rect.top + 20
+        self._sub_title_label.get_rect().centerx = screen_rect.centerx
         #Exit button
-        self.get_exit_button().get_rect().left = self.get_screen().get_rect().left + 20
-        self.get_exit_button().get_rect().centery = self.get_screen().get_rect().bottom - self.get_exit_button().get_height()
+        self.get_exit_button().get_rect().left = screen_rect.left + 20
+        self.get_exit_button().get_rect().centery = screen_rect.bottom - self.get_exit_button().get_height()
         self.get_exit_button().prep_msg(self.get_exit_button().get_text())
         #Guess button
-        self.get_guess_button().get_rect().centery = self.get_screen().get_rect().bottom - self.get_guess_button().get_height()
+        self.get_guess_button().get_rect().centery = screen_rect.bottom - self.get_guess_button().get_height()
         self.get_guess_button().prep_msg(self.get_guess_button().get_text())
         #Clear button
-        self.get_clear_button().get_rect().right = self.get_screen().get_rect().right - 20
-        self.get_clear_button().get_rect().centery = self.get_screen().get_rect().bottom - self.get_clear_button().get_height()
+        self.get_clear_button().get_rect().right =screen_rect.right - 20
+        self.get_clear_button().get_rect().centery = screen_rect.bottom - self.get_clear_button().get_height()
         self.get_clear_button().prep_msg(self.get_clear_button().get_text())
     
     #Setters and Getters
     def get_exit_button(self):
         """Returns the exit button object"""
-        return self.__exit_button
+        return self._exit_button
     def get_guess_button(self):
         """Returns the guess button object"""
-        return self.__guess_button
+        return self._guess_button
     def get_clear_button(self):
         """Returns the clear button object"""
-        return self.__clear_button
-    def get_neural_network(self):
-        """Returns the selected neural network"""
-        return self.__neural_network
-    def set_neural_network(self, neural_network:NeuralNetwork):
-        self.__neural_network = neural_network
+        return self._clear_button
     
     #Other Methods
     def check_clear_button(self, mouse_pos:()):
         """Clear image when the clear button is clicked"""
-        if gf.mouse_on_button(self.__clear_button, mouse_pos):
+        if gf.mouse_on_button(self._clear_button, mouse_pos):
             self.__colored_pixels.clear()
             self.set_screen(gf.create_screen(self.get_settings()))
     def check_exit_button(self, mouse_pos:()):
         """Exit game when exit button is clicked"""
-        if gf.mouse_on_button(self.__exit_button, mouse_pos):
+        if gf.mouse_on_button(self._exit_button, mouse_pos):
             sys.exit()  #Stops game
-    def check_guess_button(self, mouse_pos:()):
-        """Make prediction of drawn image when guess button is clicked"""
-        if self.__neural_network != None:   #Check if the neural network has been specified
-            if gf.mouse_on_button(self.__guess_button, mouse_pos):
-                self.__guess_button_pressed = True
-                #Remove the exit, guess, and clear buttons to capture only the drawn image
-                self.__buttons_to_draw.clear()
-                self.draw_components()
-                pygame.image.save(self.get_screen(), constants.pygame_image_path + constants.pygame_test_image_name)     #Take a screenshot of the game screen window and save it to disk
-                self.__buttons_to_draw = [self.__exit_button, self.__guess_button, self.__clear_button] #Add the buttons back
-                #Link to neural network prediction
-                image = file_operation.load_image(constants.pygame_image_path + constants.pygame_test_image_name)
-                image = methods.prepare_images_for_mlp_input([image])
-                prediction = self.__neural_network.predict(image[0].reshape(1, constants.image_width, 
-                                                        constants.image_height, 
-                                                        constants.color_channels), False)
-                print(prediction)
-        else:
-            print("Neural Network not yet specified")
     
     #Overridden Methods
     def check_events(self, event:Event):
@@ -105,15 +87,19 @@ class DrawPanel(Panel):
     def draw_components(self):
         super().draw_components()
         
+        #Draw sub title
+        if self._draw_sub_title_label:
+            self._sub_title_label.draw(self.get_screen())
+        
         #Color pixel
         if globals.mouse_left_pressed:
             x, y = gf.get_mouse_position()
             collide_with_button = False
-            for button in self.__buttons_to_draw:
+            for button in self._buttons_to_draw:
                 collide_with_button = button.get_rect().collidepoint(x, y)  #Check if the drawing will collide with the buttons
                 if collide_with_button:
                     break
-            if not collide_with_button and not self.__guess_button_pressed:
+            if not collide_with_button and not self._guess_button_pressed:
                 self.__colored_pixels.add((x, y))
         for pixel in self.__colored_pixels:
             pygame.draw.rect(self.get_screen(), 
@@ -124,10 +110,16 @@ class DrawPanel(Panel):
                                   self.get_settings().pen_size))
         
         #Draw buttons
-        for button in self.__buttons_to_draw:
+        for button in self._buttons_to_draw:
             button.draw_button()
         
-        if not self.__guess_button_pressed:
+        if not self._guess_button_pressed:
             pygame.display.flip()
         else:
-            self.__guess_button_pressed = False
+            self._guess_button_pressed = False
+    
+    #Abstract Methods
+    @abstractclassmethod
+    def check_guess_button(self, mouse_pos:()):
+        pass
+    
---
+++ final_project/handwritting_recognition/pygame/game_panels/draw_panel_game.py
@@ -0,0 +1,36 @@
+import pygame
+
+from final_project.handwritting_recognition import constants
+from final_project.handwritting_recognition.pygame import game_functions, \
+    globals
+from final_project.handwritting_recognition.pygame.game_panels.draw_panel import DrawPanel
+
+
+class DrawPanelGame(DrawPanel):
+    
+    #Constructor
+    def __init__(self, screen, settings):
+        super().__init__(screen, settings, "Draw a character for the AI to guess")
+        
+        self.get_exit_button().set_text("Back")
+        self.get_exit_button().prep_msg(self.get_exit_button().get_text())
+        
+        self._draw_sub_title_label = True
+    
+    #Overridden Methods
+    def check_exit_button(self, mouse_pos:()):
+        """Goes back to previous panel"""
+        if game_functions.mouse_on_button(self.get_exit_button(), mouse_pos):
+            globals.panel_index -= 1
+    
+    def check_guess_button(self, mouse_pos:()):
+        if game_functions.mouse_on_button(self.get_guess_button(), mouse_pos):
+            self._guess_button_pressed = True
+            #Remove the sub title label, exit, guess, and clear buttons to capture only the drawn image and then restore it
+            self._buttons_to_draw.clear()
+            self._draw_sub_title_label = False
+            self.draw_components()
+            pygame.image.save(self.get_screen(), constants.pygame_image_path + constants.pygame_test_image_name)     #Take a screenshot of the game screen window and save it to disk
+            self._buttons_to_draw = [self._exit_button, self._guess_button, self._clear_button] #Add the buttons back
+            self._draw_sub_title_label = True   #Draw the sub title label again
+            #TO-DO connect to loading screen
