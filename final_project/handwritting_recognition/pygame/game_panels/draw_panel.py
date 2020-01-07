1c544c5226160c9a97fc08801e1eecbb3f1c4558
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2020-01-07 10:54:19 +0700

    screenshot image now without button and label (again)
    Achieved by using a dirty trick:
    - Fill the screen with color
    - Draw the drawing

--- final_project/handwritting_recognition/pygame/game_panels/draw_panel.py
+++ final_project/handwritting_recognition/pygame/game_panels/draw_panel.py
@@ -48,15 +48,29 @@ class DrawPanel(Panel):
         self.get_clear_button().prep_msg(self.get_clear_button().get_text())
     
     #Setters and Getters
+    def set_draw_sub_title_label(self, b:bool):
+        """Method whether to draw the sub title label or not"""
+        self._draw_sub_title_label = b
+    
+    def set_guess_button_pressed(self, b:bool):
+        """Method to identify whether the guess button was pressed"""
+        self._guess_button_pressed = b
+    
     def get_exit_button(self):
         """Returns the exit button object"""
         return self._exit_button
+    
     def get_guess_button(self):
         """Returns the guess button object"""
         return self._guess_button
+    
     def get_clear_button(self):
         """Returns the clear button object"""
         return self._clear_button
+    
+    def get_buttons_to_draw(self):
+        """Returns the list of buttons that should be drawn on the screen"""
+        return self._buttons_to_draw
     
     #Other Methods
     def check_clear_button(self, mouse_pos:()):
@@ -64,10 +78,30 @@ class DrawPanel(Panel):
         if gf.mouse_on_button(self._clear_button, mouse_pos):
             self.__colored_pixels.clear()
             self.set_screen(gf.create_screen(self.get_settings()))
+            
     def check_exit_button(self, mouse_pos:()):
         """Exit game when exit button is clicked"""
         if gf.mouse_on_button(self._exit_button, mouse_pos):
             sys.exit()  #Stops game
+    
+    def draw_art(self):
+        """Method to draw the mouse click art"""
+        if globals.mouse_left_pressed:
+            x, y = gf.get_mouse_position()
+            collide_with_button = False
+            for button in self._buttons_to_draw:
+                collide_with_button = button.get_rect().collidepoint(x, y)  #Check if the drawing will collide with the buttons
+                if collide_with_button:
+                    break
+            if not collide_with_button and not self._guess_button_pressed:
+                self.__colored_pixels.add((x, y))
+        for pixel in self.__colored_pixels:
+            pygame.draw.rect(self.get_screen(), 
+                             self.get_settings().pen_color, 
+                             Rect(pixel[0], 
+                                  pixel[1], 
+                                  self.get_settings().pen_size, 
+                                  self.get_settings().pen_size))
     
     #Overridden Methods
     def check_events(self, event:Event):
@@ -86,28 +120,14 @@ class DrawPanel(Panel):
                 globals.mouse_left_pressed = False
                 
     def draw_components(self):
-        
+        super().draw_components()
         #Draw sub title
         if self._draw_sub_title_label:
             self._sub_title_label.draw(self.get_screen())
         
         #Color pixel
-        if globals.mouse_left_pressed:
-            x, y = gf.get_mouse_position()
-            collide_with_button = False
-            for button in self._buttons_to_draw:
-                collide_with_button = button.get_rect().collidepoint(x, y)  #Check if the drawing will collide with the buttons
-                if collide_with_button:
-                    break
-            if not collide_with_button and not self._guess_button_pressed:
-                self.__colored_pixels.add((x, y))
-        for pixel in self.__colored_pixels:
-            pygame.draw.rect(self.get_screen(), 
-                             self.get_settings().pen_color, 
-                             Rect(pixel[0], 
-                                  pixel[1], 
-                                  self.get_settings().pen_size, 
-                                  self.get_settings().pen_size))
+        if not self._guess_button_pressed:
+            self.draw_art()
         
         #Draw buttons
         for button in self._buttons_to_draw:
@@ -116,6 +136,8 @@ class DrawPanel(Panel):
         if not self._guess_button_pressed:
             pygame.display.flip()
         else:
+            self.get_screen().fill(self.get_settings().background_color)
+            self.draw_art()
             self._guess_button_pressed = False
     
     def reset_defaults(self):
